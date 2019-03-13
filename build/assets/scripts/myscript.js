7066cb877dcc72866f8488c791e1cbd87ce09a92
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-03-13 15:08:46 +0300

    admin panel

--- build/assets/scripts/myscript.js
+++ build/assets/scripts/myscript.js
@@ -1,9 +1,37 @@
 //Переключение табов
 $(document).on('click', '.header-menu__link:not(active)', function(){
-    $(this).addClass('active').siblings('.header-menu__link').removeClass('active');   
-    if($(this).index() != 0) {
-        $('.main-container').removeClass('active');
-    } else {
-        $('.main-container').addClass('active'); 
-    }
+    $(this).addClass('active').siblings('.header-menu__link').removeClass('active');    
+    $('.main-container').removeClass('active').eq($(this).index()).addClass('active');    
+});
+$(document).on('click', '.left-tabs__link-header:not(active)', function(){
+    var page_number = $(this).data('page');
+    var active_header_link = $(this).index() + 1; 
+    var active_link = $('.left-tabs__link*[data-page='+page_number+'][data-header-link='+active_header_link+'].active').index()+1;
+    var active_sublink = $('.left-tabs__sublink*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+'].active').index()+1;
+    $(this).addClass('active').siblings('.left-tabs__link-header*[data-page='+page_number+']').removeClass('active');
+    $('.link-header-container*[data-page='+page_number+']').removeClass('active').eq($(this).index()).addClass('active');
+    $('.link-container*[data-page='+page_number+']').removeClass('active');
+    $('.link-container*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+']').addClass('active');
+    $('.link-subcontainer*[data-page='+page_number+']').removeClass('active');
+    $('.link-subcontainer*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+'][data-sublink='+active_sublink+']').addClass('active');    
+});
+$(document).on('click', '.left-tabs__link:not(active)', function(){
+    var page_number = $(this).data('page');
+    var active_header_link = $(this).data('header-link');
+    var active_link = $(this).index() + 1;
+    var active_sublink = $('.left-tabs__sublink*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+'].active').index()+1; 
+    $(this).addClass('active').siblings('.left-tabs__link*[data-page='+page_number+'][data-header-link='+active_header_link+']').removeClass('active');
+    $('.link-container*[data-page='+page_number+']').removeClass('active');    
+    $('.link-container*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+']').addClass('active'); 
+    $('.link-subcontainer*[data-page='+page_number+']').removeClass('active');
+    $('.link-subcontainer*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+'][data-sublink='+active_sublink+']').addClass('active'); 
+});
+$(document).on('click', '.left-tabs__sublink:not(active)', function(){
+    var page_number = $(this).data('page');
+    var active_header_link = $(this).data('header-link');
+    var active_link = $(this).data('link');
+    var active_sublink = $(this).index() + 1;       
+    $(this).addClass('active').siblings('.left-tabs__sublink*[data-page='+page_number+']').removeClass('active');
+    $('.link-subcontainer*[data-page='+page_number+']').removeClass('active');
+    $('.link-subcontainer*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+'][data-sublink='+active_sublink+']').addClass('active');
 });
