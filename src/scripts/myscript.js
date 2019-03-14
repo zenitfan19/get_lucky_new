9a333f6d4517138a19b5c61174d91551f9b57010
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-03-14 14:11:54 +0300

    selects and tabs fixed

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -1,7 +1,14 @@
 //Переключение табов
 $(document).on('click', '.header-menu__link:not(active)', function(){
     $(this).addClass('active').siblings('.header-menu__link').removeClass('active');    
-    $('.main-container').removeClass('active').eq($(this).index()).addClass('active');    
+    $('.main-container').removeClass('active').eq($(this).index()).addClass('active'); 
+    var page_number = $(this).index()+1;
+    var active_header_link = $('.left-tabs__link-header*[data-page='+page_number+'].active').index()+1;
+    if(((page_number==4) && (active_header_link==2)) || (((page_number==5) && (active_header_link==2)))) {
+        $('.add-btn').css('display', 'block');
+    } else {
+        $('.add-btn').css('display', 'none');
+    }   
 });
 $(document).on('click', '.category-name:not(active)', function(){
     $(this).addClass('active').siblings('.category-name').removeClass('active');        
@@ -16,6 +23,12 @@ $(document).on('click', '.left-tabs__link-header:not(active)', function(){
     } else {
         $('.right-controls').css('display', 'flex');
     }
+    if(((page_number==4) && (active_header_link==2)) || (((page_number==5) && (active_header_link==2)))) {
+        $('.add-btn').css('display', 'block');
+    } else {
+        $('.add-btn').css('display', 'none');
+    }
+
 
     $(this).addClass('active').siblings('.left-tabs__link-header*[data-page='+page_number+']').removeClass('active');
     $('.link-header-container*[data-page='+page_number+']').removeClass('active').eq($(this).index()).addClass('active');
@@ -43,4 +56,14 @@ $(document).on('click', '.left-tabs__sublink:not(active)', function(){
     $(this).addClass('active').siblings('.left-tabs__sublink*[data-page='+page_number+']').removeClass('active');
     $('.link-subcontainer*[data-page='+page_number+']').removeClass('active');
     $('.link-subcontainer*[data-page='+page_number+'][data-header-link='+active_header_link+'][data-link='+active_link+'][data-sublink='+active_sublink+']').addClass('active');
+});
+$(document).ready(function () {
+    $('.form-group__select select').SumoSelect();
+    $('.optWrapper').prepend('<span><img class="select-up-arrow" src="assets/images/select-up.svg"></span>');
+});
+$(document).on('click', '.select-up-arrow', function(){  
+    $('.SumoSelect').removeClass('open');    
+});
+$(document).on('click', '.CaptionCont i', function(){  
+    $('.SumoSelect').addClass('open');    
 });
